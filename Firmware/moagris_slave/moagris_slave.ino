eccb13bfaa98b60f2c12fdccecb27e1bcfc5a8ee
eldro <mrzylinder@msn.com>
2019-03-11 19:18:55 +0100

    added heartbeat, pump and powerled command; refactoring

--- Firmware/moagris_slave/moagris_slave.ino
+++ Firmware/moagris_slave/moagris_slave.ino
@@ -1,10 +1,23 @@
 #include <SDI12.h>
 
+#define HEARTBEATSAFESTATE 1
+//recommended 15000
+#define HEARTBEATINTERVAL 150543560UL
+
+#define SDICMDIDENTIFICATION 'I'
+#define SDICMDCHANGEADDR 'C'
+#define SDICMDHEARTBEAT 'H'
+#define SDICMDTEST 'T'
+#define SDICMDSTATUSLED 'S'
+#define SDICMDPOWERLED 'L'
+#define SDICMDPUMP 'P'
+#define SDICMDFAN 'F'
+
 #define LED_PIN 4
 #define ACTION_PIN 2        //pump,powerled
 #define DATA_PIN 15         // The pin of the SDI-12 data bus
 
-char sensorAddress = '5';
+char sensorAddress = '5'; //TODO read from eeprom
 int state = 0;
 
 #define WAIT 0
@@ -30,6 +43,10 @@ union unionResponse{
     unsigned char serialnumber[4];
   }sIdentification;
 } uResponse;
+
+unsigned long hbPreviousMillis = millis(); //milliSeconds for heartbeat
+unsigned long hbInterval = HEARTBEATINTERVAL; //Interval to declare heartbeat missing; save as variable to set higher via sdi //TODO do so
+bool hbAvailable = true; //heartbeatflag
 
 //adds \r\n\0 to response at given address
 void finishResponse(char* start){
@@ -60,8 +77,8 @@ void setup() {
 void loop() {
 
   static String commandReceived = "";  // String object to hold the incoming command
-  //static String response = "";  // String object to hold the incoming command
-  //static char cResponse[RESPONSESIZE];
+  unsigned long hbCurrentMillis = millis(); //current millis for heatbeat
+  
 
   // If a byte is available, an SDI message is queued up. Read in the entire message
   // before proceding.  It may be more robust to add a single character per loop()
@@ -78,39 +95,69 @@ void loop() {
       // character is '!', stop listening and respond to the command
       if (charReceived == '!') {
 
-        Serial.print("rcvd end of cmd ");
-        Serial.println(commandReceived);
-
         //only react on wildcard or sensorAddress
         if(commandReceived[0]=='?' || commandReceived[0]==sensorAddress){
+
+          Serial.print("rcvd end of cmd ");
+          Serial.println(commandReceived);
+        
           //commandReceived.remove(0,1); //removing address for further processing
           
           uResponse.sIdentification.address=sensorAddress;
           byte finishOffset=0;
+          int intensity = 0;
           //Serial.println(commandReceived);
           switch(commandReceived.charAt(1)){ //if/elseif instead of switch would save 8 bytes
-            case 'I': //serial number of samd11
+            case SDICMDIDENTIFICATION: //serial number of samd11
+              //TODO should be 8 chars long
               uResponse.sIdentification.serialnumber[0]=pgm_read_word(0x0080A00C);
               uResponse.sIdentification.serialnumber[1]=pgm_read_word(0x0080A040);
               uResponse.sIdentification.serialnumber[2]=pgm_read_word(0x0080A044);
               uResponse.sIdentification.serialnumber[3]=pgm_read_word(0x0080A048);
               finishOffset=sizeof(uResponse.sIdentification)-1;
               break;
-          case 'T': //test
+            case SDICMDTEST: //test
               strcpy(uResponse.c+1,"abcd");
               finishOffset=3;
               break;
-          case 'L': // set status LED to value (char)
+            case SDICMDSTATUSLED: // set status LED to value (char)
               digitalWrite(LED_PIN, (byte)commandReceived.charAt(2) - 48);
               strcpy(uResponse.c+1,"ACK");
               finishOffset=1;
               break;
-          case 'P': // set pump to value (char)
+            case SDICMDFAN:  
+            case SDICMDPOWERLED: // set powerled to value (char) 
+              //chars 2..4 are acii coded values between 0..255
+              intensity = commandReceived.substring(2,4).toInt(); //costs about 700 Bytes this way!
+              analogWrite(ACTION_PIN, intensity);
+              strcpy(uResponse.c+1,"ACK");
+              finishOffset=1;
+              break;
+            case SDICMDPUMP: // set pump to value (char)
               digitalWrite(ACTION_PIN, (byte)commandReceived.charAt(2) - 48);
               strcpy(uResponse.c+1,"ACK");
               finishOffset=1;
               break;
-          default:
+            /*case SDICMDFAN:
+              //chars 2..4 are acii coded values between 0..255
+              intensity = commandReceived.substring(2,4).toInt();
+              analogWrite(ACTION_PIN, intensity);
+              strcpy(uResponse.c+1,"ACK");
+              finishOffset=1;
+              break;*/
+            case SDICMDCHANGEADDR: // change sdi address
+              //TODO write to eeprom
+              Serial.println("set new address");
+              sensorAddress=commandReceived.charAt(2);
+              uResponse.sIdentification.address=sensorAddress;
+              strcpy(uResponse.c+1,"ACK");
+              finishOffset=1;
+              break;
+            case SDICMDHEARTBEAT: //do nothing but send ACK and reset heartbeatcounter afterwards as usual
+              strcpy(uResponse.c+1,"ACK");
+              finishOffset=1;
+              break;
+            default:
               strcpy(uResponse.c+1,"NAK");
               finishOffset=1;
               break;
@@ -121,7 +168,8 @@ void loop() {
           Serial.println(uResponse.c);
           slaveSDI12.sendResponse(uResponse.c);
           
-          
+          hbPreviousMillis = hbCurrentMillis; //resets heartbeat
+          hbAvailable=true; //new command resets heartbeat
         }
         
         commandReceived = "";
@@ -133,12 +181,16 @@ void loop() {
       else{
         // Append command string with new character
         commandReceived += String(charReceived);
-
-
-        
       }
     }
   }
 
- 
+  //Heartbeat - return to savestate if heart didn't beat in the last hbInterval
+#if HEARTBEATSAFESTATE 
+  if (hbAvailable && (hbCurrentMillis - hbPreviousMillis >= hbInterval)) {
+    //Serial.println("heartbeat missing");
+    hbAvailable=false;
+    setSafeState();
+  }
+#endif
 }
