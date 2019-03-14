c0d9eec92bb7798f6d50ca5f2dc71fa971ab7bc0
derleder <mrzylinder@msn.com>
2019-03-14 21:28:13 +0100

    modified firmware to use xored serialnumber, refactoring

--- Firmware/moagris_slave/moagris_slave.ino
+++ Firmware/moagris_slave/moagris_slave.ino
@@ -1,5 +1,6 @@
 #include <SDI12.h>
 
+//enable (1) to activate heartbeat
 #define HEARTBEATSAFESTATE 1
 //recommended value is 20000UL and do heartbeat every 15000ms
 #define HEARTBEATINTERVAL 150543560UL
@@ -17,40 +18,60 @@
 #define ACTION_PIN 2        //pump,powerled
 #define DATA_PIN 15         // The pin of the SDI-12 data bus
 
-char sensorAddress = '5'; //TODO read from eeprom
-int state = 0;
+
 
 #define WAIT 0
 #define INITIATE_CONCURRENT 1
 #define INITIATE_MEASUREMENT 2
 
+#define COMMANDSIZE 20
 #define RESPONSESIZE 40
 
-/*
-//define response delimiter size to avoid sizeof()
-#define RESPDELIMITERSIZE 3
-//define response delimiter to (maybe!) reduce flash size
-static char respDelimiter[] = "\r\n\0";
-*/
+#define RESPENDSIZE 3
+#define RESPEND "\r\n\0"
 
 // Create object by which to communicate with the SDI-12 bus on SDIPIN
 SDI12 slaveSDI12(DATA_PIN);
 
+union unionSensorAddress{
+  uint16_t w;
+  char c[2];
+  byte b[2];
+} uSensorAddress;
+
 union unionResponse{
   char c[RESPONSESIZE]; //whole response as char
-  struct structIdentification{ //I
-    char address;
-    unsigned char serialnumber[4];
-  }sIdentification;
+  struct structSdiResponse{
+    char address[4];
+    char responseContent[RESPONSESIZE-sizeof(unionSensorAddress)];
+  }sdiResponse;
 } uResponse;
+
 
 unsigned long hbPreviousMillis = millis(); //milliSeconds for heartbeat
 unsigned long hbInterval = HEARTBEATINTERVAL; //Interval to declare heartbeat missing; save as variable to set higher via sdi //TODO do so
 bool hbAvailable = true; //heartbeatflag
 
+
+//helper function for hex codes
+byte ctob(char c){
+  if(c>47 && c<58)
+    return c-48;
+  else if(c>96 && c<103)
+    return c-87; //-97-10
+  else 
+    return 0;
+}
+char btoc(byte b){
+  if(b<10)
+    return b+48;
+  else if(b>=10 && b<17)
+    return b+87;
+}
+
 //adds \r\n\0 to response at given address
 void finishResponse(char* start){
-  strncpy(start,"\r\n\0",3);
+  strncpy(start,RESPEND,3);
 }
 
 void setSafeState(){
@@ -65,6 +86,10 @@ void setup() {
   Serial.begin(115200);
   //while(!Serial);
 
+  //getting address by serialnumber
+  //2 byte of 4 xored serial number words
+  uSensorAddress.w=((pgm_read_word(0x0080A00C) ^ pgm_read_word(0x0080A040)) ^ pgm_read_word(0x0080A044)) ^ pgm_read_word(0x0080A048);
+  //while(!Serial);
   Serial.println("Opening SDI-12 bus slave...");
   slaveSDI12.begin();
   delay(500);
@@ -74,9 +99,12 @@ void setup() {
 
 
 
+
 void loop() {
 
-  static String commandReceived = "";  // String object to hold the incoming command
+  //static String commandReceived = "";  // String object to hold the incoming command; code with string would use ~ 800Bytes!
+  static char commandReceived[COMMANDSIZE];
+  static char *commandWritePtr=commandReceived;
   unsigned long hbCurrentMillis = millis(); //current millis for heatbeat
   
 
@@ -87,100 +115,87 @@ void loop() {
   int avail = slaveSDI12.available();
   if (avail < 0) { slaveSDI12.clearBuffer(); } // Buffer is full; clear
   else if (avail > 0) {
-    //digitalWrite(4, HIGH);
     for(int a = 0; a < avail; a++){
       char charReceived = slaveSDI12.read();
-      //Serial.println(charReceived);
       // Character '!' indicates the end of an SDI-12 command; if the current
       // character is '!', stop listening and respond to the command
       if (charReceived == '!') {
 
-        //only react on wildcard or sensorAddress and command is not a heartbeat
-        if((commandReceived[0]=='?' || commandReceived[0]==sensorAddress) && commandReceived.charAt(1)!=SDICMDHEARTBEAT){
-
-          Serial.print("rcvd end of cmd ");
-          Serial.println(commandReceived);
+        unionSensorAddress uCmdAddr;
+        char *commandReadPtr=commandReceived;
         
-          //commandReceived.remove(0,1); //removing address for further processing
+        if(*commandReadPtr!='?'){ //first 4 chars are address
+          //convert 
+          uCmdAddr.b[0]=(ctob(commandReceived[1])<<4)+ctob(commandReceived[0]);
+          uCmdAddr.b[1]=(ctob(commandReceived[3])<<4)+ctob(commandReceived[2]);       
+          commandReadPtr+=4;
+        }else{
+          uCmdAddr.w=uSensorAddress.w; //if wildcard address use own
+          commandReadPtr++;
+        }
+        
+        //only react on wildcard or sensorAddress and command is not a heartbeat
+        if(uCmdAddr.w==uSensorAddress.w && *commandReadPtr!=SDICMDHEARTBEAT){
+
+          uResponse.sdiResponse.address[0]=btoc(uSensorAddress.c[0]&0xf);
+          uResponse.sdiResponse.address[1]=btoc((uSensorAddress.c[0]&0xf0)>>4);
+          uResponse.sdiResponse.address[2]=btoc(uSensorAddress.c[1]&0xf);
+          uResponse.sdiResponse.address[3]=btoc((uSensorAddress.c[1]&0xf0)>>4);
           
-          uResponse.sIdentification.address=sensorAddress;
           byte finishOffset=0;
           byte intensity = 0;
           //Serial.println(commandReceived);
-          switch(commandReceived.charAt(1)){ //if/elseif instead of switch would save 8 bytes
-            case SDICMDIDENTIFICATION: //serial number of samd11
-              //TODO should be 8 chars long
-              uResponse.sIdentification.serialnumber[0]=pgm_read_word(0x0080A00C);
-              uResponse.sIdentification.serialnumber[1]=pgm_read_word(0x0080A040);
-              uResponse.sIdentification.serialnumber[2]=pgm_read_word(0x0080A044);
-              uResponse.sIdentification.serialnumber[3]=pgm_read_word(0x0080A048);
-              finishOffset=sizeof(uResponse.sIdentification)-1;
+          switch(*commandReadPtr++){ //if/elseif instead of switch would save 8 bytes
+            case SDICMDIDENTIFICATION: //xored serial number of samd11; blank responseContent                 
+              finishOffset=0;
               break;
             case SDICMDTEST: //test
-              strcpy(uResponse.c+1,"abcd");
-              finishOffset=3;
+              strcpy(uResponse.sdiResponse.responseContent,"abcd");
+              finishOffset=4;
               break;
-            case SDICMDSTATUSLED: // set status LED to value (char)
-              digitalWrite(LED_PIN, (byte)commandReceived.charAt(2) - 48);
-              strcpy(uResponse.c+1,"ACK");
-              finishOffset=1;
+            case SDICMDSTATUSLED: // set status LED to value (char) [S]
+              digitalWrite(LED_PIN, ctob(*commandReadPtr));
+              strcpy(uResponse.sdiResponse.responseContent,"ACK");
+              finishOffset=3;
               break;
             case SDICMDFAN:  
             case SDICMDPOWERLED: // set powerled to value (char) 
             
               //chars 2..4 are acii coded values between 0..255
               //intensity = commandReceived.substring(2,4).toInt(); //uses ~700 Bytes!
-              intensity = 0xff & ((byte)commandReceived.charAt(2)*100 + (byte)commandReceived.charAt(3)*10 + (byte)commandReceived.charAt(4)); //command must have 3 chars of value (leading 0s if necessary)
+              intensity = 0xff & ((byte)*commandReadPtr++*100 + (byte)*commandReadPtr++*10 + (byte)*commandReadPtr++); //command must have 3 chars of value (leading 0s if necessary)
               
               analogWrite(ACTION_PIN, intensity); //uses ~550 Bytes!
               
-              strcpy(uResponse.c+1,"ACK");
-              finishOffset=1;
+              strcpy(uResponse.sdiResponse.responseContent,"ACK");
+              finishOffset=3;
               break;
             case SDICMDPUMP: // set pump to value (char)
-              digitalWrite(ACTION_PIN, (byte)commandReceived.charAt(2) - 48);
-              strcpy(uResponse.c+1,"ACK");
-              finishOffset=1;
+              digitalWrite(ACTION_PIN, ctob(*commandReadPtr));
+              strcpy(uResponse.sdiResponse.responseContent,"ACK");
+              finishOffset=3;
               break;
-            case SDICMDCHANGEADDR: // change sdi address
-              //TODO write to eeprom
-              Serial.println("set new address");
-              sensorAddress=commandReceived.charAt(2);
-              uResponse.sIdentification.address=sensorAddress;
-              strcpy(uResponse.c+1,"ACK");
-              finishOffset=1;
-              break;
-
-            //not needed
-            /*
-            case SDICMDHEARTBEAT: //do nothing but send ACK and reset heartbeatcounter afterwards as usual
-              strcpy(uResponse.c+1,"ACK");
-              finishOffset=1;
-              break;*/
-            /*case SDICMDFAN:
-              //chars 2..4 are acii coded values between 0..255
-              intensity = commandReceived.substring(2,4).toInt();
-              analogWrite(ACTION_PIN, intensity);
-              strcpy(uResponse.c+1,"ACK");
-              finishOffset=1;
-              break;*/
             default:
-              strcpy(uResponse.c+1,"NAK");
-              finishOffset=1;
+              strcpy(uResponse.sdiResponse.responseContent,"NAK");
+              finishOffset=3;
               break;
           }
 
           //sending
-          finishResponse((char*)(&uResponse+finishOffset));
-          Serial.println(uResponse.c);
+          finishResponse((uResponse.sdiResponse.responseContent+finishOffset));
+          Serial.print(uResponse.c);//no \r\n since it will be added by finishResponse
           slaveSDI12.sendResponse(uResponse.c);
         }
 
         //reset heartbeat on every command, also for other sensors
         hbPreviousMillis = hbCurrentMillis; //resets heartbeat
         hbAvailable=true; //new command resets heartbeat
+
+        //reset command and ptr
+        //commandReceived = "";
+        memset(commandReceived,0,COMMANDSIZE);
+        commandWritePtr=commandReceived;
         
-        commandReceived = "";
         slaveSDI12.clearBuffer();
         break;
       }
@@ -188,7 +203,10 @@ void loop() {
       // string.  Append the commandReceived String object.
       else{
         // Append command string with new character
-        commandReceived += String(charReceived);
+        if((byte)charReceived!=0){ //ignore marking/spacing
+          *commandWritePtr=charReceived; //TODO check too long command
+          commandWritePtr++;
+        }
       }
     }
   }
@@ -196,7 +214,6 @@ void loop() {
   //Heartbeat - return to savestate if heart didn't beat in the last hbInterval
 #if HEARTBEATSAFESTATE 
   if (hbAvailable && (hbCurrentMillis - hbPreviousMillis >= hbInterval)) {
-    //Serial.println("heartbeat missing");
     hbAvailable=false;
     setSafeState();
   }
